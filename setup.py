7b635ef6369f4f7b9a418f3f5b25b5dcf6473197
Jesse Almanrode <jesse@almanrode.com>
2023-02-12 16:49:58 -0700

    Setup management of dependencies using Poetry.

--- setup.py
+++ setup.py
@@ -1,34 +1,7 @@
 # coding=utf-8
-"""Setup file for lazy-hippo utility"""
+""" Setup.py stub for lazy-hippo
+"""
 
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(name='lazy-hippo',
-      version='0.2.1',
-      author='Jesse Almanrode',
-      author_email='jesse@almanrode.com',
-      description='Utility splitting video files by timestamp',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      py_modules=['lazy_hippo'],
-      python_requires='>=3.10',
-      install_requires=['click>=8.1.3',
-                        'colorama>=0.4.5'
-                        ],
-      entry_points={
-          'console_scripts': [
-              'lazy-hippo = lazy_hippo:cli',
-          ]
-      },
-      platforms=['Linux', 'Darwin'],
-      classifiers=[
-          'Programming Language :: Python',
-          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-          'Development Status :: 5 - Production/Stable',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-      ],
-      )
+setup()
